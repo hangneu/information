fbd0a9656ac6ce511244198490dd54da6b2acb09
Hang Shang <shang.h@dmc38c98602148f.nunet.neu.edu>
2016-10-15 13:46:23 -0400

    1000 done

--- pr.py
+++ pr.py
@@ -6,8 +6,8 @@ def count(file):
 		i = i + 1
 		d[a.split('\n')[0].split(" ")[0]] = 1
 	return i,d
-def sink_set():
-	file_out_link = open("out_test.txt","r")
+def sink_set(out_link):
+	file_out_link = open(out_link,"r")
 	sink_set = []
 	for a in file_out_link:
 		if(len(a.split("\n")[0].split(" ")) <= 2):
@@ -16,8 +16,8 @@ def sink_set():
 	for a in sink_set:
 		sink = sink + d[a]
 	return sink
-def sink_num():
-	file_out_link = open("out_test.txt","r")
+def sink_num(out_link):
+	file_out_link = open(out_link,"r")
 	d = {}
 	for a in file_out_link:
 		count = 0
@@ -30,13 +30,13 @@ def perp(d):
 		en += d[a]*math.log(2,d[a])
 	per = 2**(en*(-1.0))
 	return per
-def pr():
-	file = open("test.txt",'r')
+def pr(in_link,out_link):
+	file = open(in_link,'r')
 	(N,d) = count(file)
 	file.close()
 	for a in d:
 		d[a] = 1.0/N
-	sink = sink_set()
+	sink = sink_set(out_link)
 	time = 0
 	present = 9999
 	count_1 = 1
@@ -45,12 +45,16 @@ def pr():
 		count_1 += 1
 		new_d = {}
 		pre = present
-		out_num = sink_num()
-		file_in_link = open("test.txt","r")
+		out_num = sink_num(out_link)
+		file_in_link = open(in_link,"r")
 		for a in file_in_link:
 			new_d[a.split("\n")[0].split(" ")[0]] = (1-0.85)/N
 			new_d[a.split("\n")[0].split(" ")[0]] += 0.85*sink/N
-			b = a.split("\n")[0].split(" ")[1:]
+			b = a.split("\n")[0].split(" ")
+			if (in_link!="test.txt"):
+				b = b[1:len(b)-1]
+			else:
+				b = b[1:]
 			for item in b:
 				new_d[a.split("\n")[0].split(" ")[0]] += 0.85*d[item]/out_num[item]
 		d = new_d
@@ -58,8 +62,26 @@ def pr():
 		if(abs(present-pre)<1.0):
 			time += 1
 	return d
-d = pr()
-print d
+in_link = "test.txt"
+out_link = "out_test.txt"
+in_1000 = "ass_first.txt"
+out_1000 = "out_link_1000.txt"
+d = pr(in_1000,out_1000)
+# d = pr(in_link,out_link)
+dict1 = sorted(d.iteritems(),key=lambda d:d[1],reverse = True)
+for item in dict1:
+	print item#,dict1[item]
+
+
+
+
+
+
+
+
+
+
+
 
 
 
